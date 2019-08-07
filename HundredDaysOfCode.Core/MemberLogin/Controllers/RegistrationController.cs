1867e91d0a039043dd6d5d13ea577164d284df91
jmayntzhusen <jmh@umbraco.com>
2019-08-07 20:57:38 +0200

    Add registration controller

---
+++ HundredDaysOfCode.Core/MemberLogin/Controllers/RegistrationController.cs
@@ -0,0 +1,35 @@
+﻿using HundredDaysOfCode.Core.MemberLogin.Models;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace HundredDaysOfCode.Core.MemberLogin.Controllers
+{
+    public class RegistrationController : SurfaceController
+    {
+        public ActionResult Index()
+        {
+            return PartialView("Registration", new RegistrationModel());
+        }
+
+        [HttpPost] 
+        public ActionResult Submit(RegistrationModel model)
+        {
+            if (!ModelState.IsValid)
+                return CurrentUmbracoPage();
+
+            var ms = Services.MemberService;
+            if (ms.GetByEmail(model.Email) != null)
+            {
+                ModelState.AddModelError("exists", "This email is taken.");
+                return CurrentUmbracoPage();
+            }
+
+            var member = ms.CreateMemberWithIdentity(model.Email, model.Email, model.Email, "Member");
+            ms.Save(member);
+            ms.SavePassword(member, model.Password);
+            Members.Login(model.Email, model.Password);
+
+            return Redirect("/profile");
+        }
+    }
+}
---
+++ HundredDaysOfCode.Core/MemberLogin/Models/RegistrationModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HundredDaysOfCode.Core.MemberLogin.Models
+{
+    public class RegistrationModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        public string Password { get; set; }
+    }
+}
